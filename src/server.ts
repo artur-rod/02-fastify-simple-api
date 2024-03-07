6b12e3cd6b8907953cf43dd3191f4ef138830280
artur-rod <tui-dtj@hotmail.com>
2024-03-07 08:19:31 -0300

    chore: :rocket: Deploy configurations

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,9 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: env.PORT }).then(() => console.log('Server a milhão!'))
+app
+  .listen({
+    port: env.PORT,
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
+  })
+  .then(() => console.log('Server a milhão!'))
